fbc594f3bbe30df0c7a56647a9d9c53072312bfc
Rizky Fadillah <fadillahrizki16@gmail.com>
2024-10-18 07:02:50 +0700

    feat: update color homepage

--- src/features/homepage/jurusan.tsx
+++ src/features/homepage/jurusan.tsx
@@ -2,26 +2,26 @@ import { UserIcon } from "@heroicons/react/16/solid"
 
 export const Jurusan = () => {
   return (
-    <div className='py-16 px-6 md:px-24 lg:px-32 bg-indigo-600 grid grid-cols-2 gap-4 lg:grid-cols-4'>
+    <div className='py-16 px-6 md:px-24 lg:px-32 bg-orange-300 grid grid-cols-2 gap-4 lg:grid-cols-4'>
         <div className="flex flex-col items-center">
-            <UserIcon className="w-24" color="white" />
-            <h5 className="mb-1 text-4xl font-medium text-white">120</h5>
-            <span className="text-xl font-medium text-white">Teknik Komputer Jaringan</span>
+            <UserIcon className="w-24 text-gray-700" />
+            <h5 className="mb-1 text-4xl font-medium text-gray-700">120</h5>
+            <span className="text-xl font-medium text-gray-700">Teknik Komputer Jaringan</span>
         </div>
         <div className="flex flex-col items-center">
-            <UserIcon className="w-24" color="white" />
-            <h5 className="mb-1 text-4xl font-medium text-white">120</h5>
-            <span className="text-xl font-medium text-white">Teknik Komputer Jaringan</span>
+            <UserIcon className="w-24 text-gray-700" />
+            <h5 className="mb-1 text-4xl font-medium text-gray-700">120</h5>
+            <span className="text-xl font-medium text-gray-700">Teknik Komputer Jaringan</span>
         </div>
         <div className="flex flex-col items-center">
-            <UserIcon className="w-24" color="white" />
-            <h5 className="mb-1 text-4xl font-medium text-white">120</h5>
-            <span className="text-xl font-medium text-white">Teknik Komputer Jaringan</span>
+            <UserIcon className="w-24 text-gray-700" />
+            <h5 className="mb-1 text-4xl font-medium text-gray-700">120</h5>
+            <span className="text-xl font-medium text-gray-700">Teknik Komputer Jaringan</span>
         </div>
         <div className="flex flex-col items-center">
-            <UserIcon className="w-24" color="white" />
-            <h5 className="mb-1 text-4xl font-medium text-white">120</h5>
-            <span className="text-xl font-medium text-white">Teknik Komputer Jaringan</span>
+            <UserIcon className="w-24 text-gray-700" />
+            <h5 className="mb-1 text-4xl font-medium text-gray-700">120</h5>
+            <span className="text-xl font-medium text-gray-700">Teknik Komputer Jaringan</span>
         </div>
     </div>
   )
--- src/features/homepage/statistics.tsx
+++ src/features/homepage/statistics.tsx
@@ -2,26 +2,26 @@ import { UserIcon } from "@heroicons/react/16/solid"
 
 export const Statistics = () => {
   return (
-    <div className='py-16 px-6 md:px-24 lg:px-32 bg-green-400 grid grid-cols-2 gap-4 lg:grid-cols-4'>
+    <div className='py-16 px-6 md:px-24 lg:px-32 bg-green-300 grid grid-cols-2 gap-4 lg:grid-cols-4'>
         <div className="flex flex-col items-center">
-            <UserIcon className="w-24" color="white" />
-            <h5 className="mb-1 text-4xl font-medium text-white">120</h5>
-            <span className="text-xl font-medium text-white">Siswa</span>
+            <UserIcon className="w-24 text-gray-700"/>
+            <h5 className="mb-1 text-4xl font-medium text-gray-700">120</h5>
+            <span className="text-xl font-medium text-gray-700">Siswa</span>
         </div>
         <div className="flex flex-col items-center">
-            <UserIcon className="w-24" color="white" />
-            <h5 className="mb-1 text-4xl font-medium text-white">120</h5>
-            <span className="text-xl font-medium text-white">Guru</span>
+            <UserIcon className="w-24 text-gray-700"/>
+            <h5 className="mb-1 text-4xl font-medium text-gray-700">120</h5>
+            <span className="text-xl font-medium text-gray-700">Guru</span>
         </div>
         <div className="flex flex-col items-center">
-            <UserIcon className="w-24" color="white" />
-            <h5 className="mb-1 text-4xl font-medium text-white">120</h5>
-            <span className="text-xl font-medium text-white">Alumni</span>
+            <UserIcon className="w-24 text-gray-700"/>
+            <h5 className="mb-1 text-4xl font-medium text-gray-700">120</h5>
+            <span className="text-xl font-medium text-gray-700">Alumni</span>
         </div>
         <div className="flex flex-col items-center">
-            <UserIcon className="w-24" color="white" />
-            <h5 className="mb-1 text-4xl font-medium text-white">120</h5>
-            <span className="text-xl font-medium text-white">Staff</span>
+            <UserIcon className="w-24 text-gray-700"/>
+            <h5 className="mb-1 text-4xl font-medium text-gray-700">120</h5>
+            <span className="text-xl font-medium text-gray-700">Staff</span>
         </div>
     </div>
   )
