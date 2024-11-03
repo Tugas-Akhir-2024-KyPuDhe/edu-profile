106badeda664878a5690c7f5ca7984174c17e945
Rizky Fadillah <fadillahrizki16@gmail.com>
2024-11-03 21:20:23 +0700

    feat: add env to gitignore

--- src/features/konten/berita/news.tsx
+++ src/features/konten/berita/news.tsx
@@ -1,6 +1,19 @@
+import { useEffect } from 'react'
 import { MyCard } from '../../../components'
+import { ArticleService } from '../../../services'
 
 export const News = () => {
+
+  useEffect(()=>{
+    created()
+  }, [])
+
+  async function created() {
+    const response = await ArticleService().all();
+
+    console.log(response)
+  }
+
   return (
     <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         <MyCard />
