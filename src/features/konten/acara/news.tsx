2e6bf03a4b1f67d034597853bc3d532fb10d5682
Rizky Fadillah <fadillahrizki16@gmail.com>
2024-11-03 23:44:24 +0700

    feat: fetch from backend

--- src/features/konten/acara/news.tsx
+++ src/features/konten/acara/news.tsx
@@ -1,16 +1,28 @@
+import { useEffect, useState } from 'react'
 import { MyCard } from '../../../components'
+import { ArticleService } from '../../../services'
+import { Article } from '../../../interfaces'
 
 export const News = () => {
+
+  const [data, setData] = useState<Article[]>([])
+
+  useEffect(()=>{
+    created()
+  }, [])
+
+  async function created() {
+    try {
+      const response = await ArticleService().all()
+      setData(response.data)
+    } catch (error) {
+      console.error(error)      
+    }
+  }
+
   return (
     <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        <MyCard />
-        <MyCard />
-        <MyCard />
-        <MyCard />
-        <MyCard />
-        <MyCard />
-        <MyCard />
-        <MyCard />
+      {data.map(article=><MyCard key={article.id} image={"https://flowbite-react.com/images/blog/image-1.jpg"} title={article.title} description={article.description} href={'/konten/acara/'+article.id} badge={article.type}/>)}
     </div>
   )
 }
