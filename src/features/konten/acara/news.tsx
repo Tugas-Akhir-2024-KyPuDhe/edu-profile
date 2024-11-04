4100482e5bc7d295523c271835ca79e9e4654d96
Rizky Fadillah <fadillahrizki16@gmail.com>
2024-11-04 17:51:09 +0700

    feat: add loading to article

--- src/features/konten/acara/news.tsx
+++ src/features/konten/acara/news.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { MyCard } from '../../../components'
 import { ArticleService } from '../../../services'
 import { Article } from '../../../interfaces'
+import { Spinner } from 'flowbite-react'
 
 export const News = () => {
 
@@ -20,9 +21,13 @@ export const News = () => {
     }
   }
 
-  return (
+  return data.length ? (
     <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {data.map(article=><MyCard key={article.id} image={"https://flowbite-react.com/images/blog/image-1.jpg"} title={article.title} description={article.description} href={'/konten/acara/'+article.id} badge={article.type}/>)}
+    </div>
+  ) : (
+    <div className="m-auto">
+      <Spinner size="lg"/>
     </div>
   )
 }
