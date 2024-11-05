516a7b3256d129f412ddfb17c665dd4bcbc610a7
Rizky Fadillah <fadillahrizki16@gmail.com>
2024-11-05 17:15:46 +0700

    feat: change news homepage

--- src/features/homepage/index.ts
+++ src/features/homepage/index.ts
@@ -1,7 +1,6 @@
 import { Jurusan } from './jurusan';
 import { MyCarousel } from './carousel';
 import { Statistics } from './statistics';
-import { News } from './news';
 import { MyBanner } from './banner';
 
-export { MyBanner, News, Statistics, MyCarousel, Jurusan }
+export { MyBanner, Statistics, MyCarousel, Jurusan }
--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -1,5 +1,6 @@
 import {Header, MyFooter} from "../../components";
-import {Jurusan, MyBanner, MyCarousel, News, Statistics} from "../../features/homepage";
+import {Jurusan, MyBanner, MyCarousel, Statistics} from "../../features/homepage";
+import { News } from "../../features/konten/berita";
 
 export const HomePage = () => {
   return (
@@ -9,7 +10,12 @@ export const HomePage = () => {
       <Statistics />
       <MyBanner />
       <Jurusan />
-      <News />
+
+      <div className="container py-10 gap-10 flex flex-col min-h-[40vh]">
+        <h2 className="text-3xl font-bold text-gray-900 text-center">Berita Terbaru</h2>
+        <News />
+      </div>
+      
       <MyFooter />
     </>
   );
