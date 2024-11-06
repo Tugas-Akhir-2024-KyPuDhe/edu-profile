68210207bb8e255bed22dcd66663dcca4cc2fbb1
Rizky Fadillah <fadillahrizki16@gmail.com>
2024-11-06 16:57:14 +0700

    feat: change some features

--- src/features/homepage/jurusan.tsx
+++ src/features/homepage/jurusan.tsx
@@ -1,28 +1,48 @@
-import { UserIcon } from "@heroicons/react/16/solid"
+import { useEffect, useRef, useState } from "react"
+import { Fajusek } from "../../interfaces"
+import { JurusanService } from "../../services"
+import { Spinner } from "flowbite-react"
+import { useIsVisible } from "../../utils"
 
 export const Jurusan = () => {
-  return (
-    <div className='py-16 px-6 md:px-24 lg:px-32 bg-orange-300 grid grid-cols-2 gap-4 lg:grid-cols-4'>
-        <div className="flex flex-col items-center">
-            <UserIcon className="w-24 text-gray-700" />
-            <h5 className="mb-1 text-4xl font-medium text-gray-700">120</h5>
-            <span className="text-xl font-medium text-gray-700">Teknik Komputer Jaringan</span>
-        </div>
-        <div className="flex flex-col items-center">
-            <UserIcon className="w-24 text-gray-700" />
-            <h5 className="mb-1 text-4xl font-medium text-gray-700">120</h5>
-            <span className="text-xl font-medium text-gray-700">Teknik Komputer Jaringan</span>
-        </div>
-        <div className="flex flex-col items-center">
-            <UserIcon className="w-24 text-gray-700" />
-            <h5 className="mb-1 text-4xl font-medium text-gray-700">120</h5>
-            <span className="text-xl font-medium text-gray-700">Teknik Komputer Jaringan</span>
-        </div>
-        <div className="flex flex-col items-center">
-            <UserIcon className="w-24 text-gray-700" />
-            <h5 className="mb-1 text-4xl font-medium text-gray-700">120</h5>
-            <span className="text-xl font-medium text-gray-700">Teknik Komputer Jaringan</span>
-        </div>
+
+    const [data, setData] = useState<Fajusek[]>([])
+    const [isLoading, setIsLoading] = useState(true)
+
+    const ref1 = useRef(null)
+    const isVisible1 = useIsVisible(ref1)
+    
+    useEffect(()=>{
+        loadData()
+
+        async function loadData() {
+        setIsLoading(true)
+        try {
+            const response = await JurusanService().all()
+            setData(response.data)
+        } catch (error) {
+            console.error(error)      
+        }
+        setIsLoading(false)
+        }
+    }, [])
+
+    return <div ref={ref1} className={`transition ease-in duration-700 ${isVisible1 ? "opacity-100 translate-y-0" : "opacity-0 translate-y-6"}`}>
+        {isLoading ? (
+            <div className="m-auto">
+                <Spinner size="lg"/>
+            </div>
+        ) : (
+            <div className="container gap-10 flex flex-col py-20">
+                <h2 className="text-3xl font-bold text-center uppercase text-gray-900">Jurusan</h2>
+
+                {data.map(jurusan=>(
+                    <div key={jurusan.id}>
+                        <h3 className="text-lg font-medium">{jurusan.name}</h3>
+                        <p>{jurusan.description}</p>
+                    </div>
+                ))}
+            </div>
+        )}
     </div>
-  )
 }
--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -1,18 +1,45 @@
+import { useEffect, useRef, useState } from "react";
 import {Header, MyFooter} from "../../components";
-import {Jurusan, MyBanner, MyCarousel, Statistics} from "../../features/homepage";
+import {Jurusan, MyCarousel, Statistics} from "../../features/homepage";
 import { News } from "../../features/konten/berita";
+import { School } from "../../interfaces";
+import { useIsVisible } from "../../utils";
 
 export const HomePage = () => {
+
+  const [schoolConfig, setSchoolConfig] = useState<School>()
+
+  const ref1 = useRef(null)
+  const isVisible1 = useIsVisible(ref1)
+
+  const ref2 = useRef(null)
+  const isVisible2 = useIsVisible(ref2)
+
+  useEffect(()=> {
+    if(localStorage.getItem("school_config")) {
+      setSchoolConfig(JSON.parse(localStorage.getItem("school_config")!))
+    }
+  }, [])
+
   return (
     <>
       <Header />
       <MyCarousel />
+      
+      <div ref={ref1} className={`container gap-10 flex flex-col py-20 transition ease-in duration-700 ${isVisible1 ? "opacity-100 translate-y-0" : "opacity-0 translate-y-6"}`}>
+
+        <h2 className="text-3xl font-bold text-center uppercase text-gray-900">Sejarah Singkat</h2>
+        <p className="text-lg leading-8 text-gray-900 text-center">
+            {schoolConfig?.about}
+        </p>
+
+      </div>
+
       <Statistics />
-      <MyBanner />
       <Jurusan />
 
-      <div className="container py-10 gap-10 flex flex-col ">
-        <h2 className="text-3xl font-bold text-gray-900 text-center">Berita Terbaru</h2>
+      <div ref={ref2} className={`container gap-10 flex flex-col py-20 transition ease-in duration-700 ${isVisible2 ? "opacity-100 translate-y-0" : "opacity-0 translate-y-6"}`}>
+        <h2 className="text-3xl font-bold text-center uppercase text-gray-900">Berita Terbaru</h2>
         <News />
       </div>
       
