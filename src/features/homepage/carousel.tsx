eeee4479cb88bff24e0748f6805307e94403092c
Rizky Fadillah <fadillahrizki16@gmail.com>
2024-11-06 17:54:15 +0700

    feat: change carousel content

--- src/features/homepage/carousel.tsx
+++ src/features/homepage/carousel.tsx
@@ -6,9 +6,15 @@ export const MyCarousel = ()=> {
       <Carousel slideInterval={5000} theme={{scrollContainer : {
         base: "flex h-full snap-mandatory overflow-y-hidden overflow-x-scroll scroll-smooth"
       }}}>
-        <img src="https://i.pinimg.com/736x/54/ce/39/54ce39d259347c487215acd36663d4c7.jpg" alt="Carousel 1" />
-        <img src="https://i.pinimg.com/736x/3b/0b/bc/3b0bbc9d75b1925578dba899b530a270.jpg" alt="Carousel 2" />
-        <img src="https://i.pinimg.com/736x/4f/4b/9d/4f4b9d8340fa6db8be27dfbcc11cd109.jpg" alt="Carousel 3" />
+        <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
+          Content 1
+        </div>
+        <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
+          Content 2
+        </div>
+        <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
+          Content 3
+        </div>
       </Carousel>
     </div>
   );
