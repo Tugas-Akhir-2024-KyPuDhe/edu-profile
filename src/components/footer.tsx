547b9094e987f416ed28a4362b069d8dd7105aec
Rizky Fadillah <fadillahrizki16@gmail.com>
2024-12-03 12:24:51 +0700

    fix: footer sosmed

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -28,8 +28,8 @@ export function MyFooter() {
             <h4>{schoolConfig?.address}</h4>
             <a href={"tel:"+schoolConfig?.telp} className="flex gap-2 items-center text-sm text-gray-500 hover:underline"> <FaPhone size={20} />  {schoolConfig?.telp}</a>
             <a href={"mailto:"+schoolConfig?.email} className="flex gap-2 items-center text-sm text-gray-500 hover:underline"><FaEnvelope size={20} />{schoolConfig?.email}</a>
-            <a href={"https://www.facebook.com/"+schoolConfig?.fb} className="flex gap-2 items-center text-sm text-gray-500 hover:underline"> <FaFacebook size={20} />  {schoolConfig?.name}</a>
-            <a href={"https://www.instagram.com/"+schoolConfig?.ig} className="flex gap-2 items-center text-sm text-gray-500 hover:underline"> <FaInstagram size={20} />{schoolConfig?.name}</a>
+            <a href={schoolConfig?.fb} className="flex gap-2 items-center text-sm text-gray-500 hover:underline"> <FaFacebook size={20} />  {schoolConfig?.name}</a>
+            <a href={schoolConfig?.ig} className="flex gap-2 items-center text-sm text-gray-500 hover:underline"> <FaInstagram size={20} />{schoolConfig?.name}</a>
           </div>
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3985.7082479234086!2d99.01353117567656!3d2.6008966562766807!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3031f01ad5a61e53%3A0xa1341928e9376e81!2sSMK%20Negeri%201%20Lumbanjulu!5e0!3m2!1sen!2sid!4v1730804530458!5m2!1sen!2sid" className="w-full sm:w-1/2 md:w-1/3 h-80" loading="lazy"></iframe>
         </div>
