1e94fb6ce30808972f7f1f349d9372c7bc0e176b
Rizky Fadillah <fadillahrizki16@gmail.com>
2024-12-31 12:21:21 +0700

    add light gallery

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
+// import styles
+import 'lightgallery/css/lightgallery.css';
+import 'lightgallery/css/lg-zoom.css';
+import 'lightgallery/css/lg-thumbnail.css';
+
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <App />
--- src/pages/eksul/index.tsx
+++ src/pages/eksul/index.tsx
@@ -2,12 +2,15 @@ import { useEffect, useState } from "react";
 import {MyBanner} from "../../components";
 import { Fajusek } from "../../interfaces";
 import { EkstrakurikulerService } from "../../services";
-import { Spinner } from "flowbite-react";
+
+import { Sidebar, Spinner } from "flowbite-react";
+import LightGallery from 'lightgallery/react';
 
 export const Ekskul = () => {
 
   const [data, setData] = useState<Fajusek[]>([])
   const [isLoading, setIsLoading] = useState(true)
+  const [activeTab, setActiveTab] = useState(0)
   
   useEffect(()=>{
     loadData()
@@ -28,20 +31,41 @@ export const Ekskul = () => {
     <>
         <MyBanner title="Ekstrakurikuler" currentPage="Ekstrakurikuler" beforePages={[{title: "Beranda", href: "/"}]} />
 
-        <div className="container py-10 gap-10 flex flex-col ">
-          {isLoading ? (
+        <div className="container py-10 gap-10 flex flex-col">
+
+        {isLoading ? (
             <div className="m-auto">
               <Spinner size="lg"/>
             </div>
           ) : (
-            data.map(ekskul=>(
-              <div className="flex flex-col gap-3">
-                <h2 className="text-lg font-medium">{ekskul.name}</h2>
-                <p>{ekskul.description}</p>
-                {ekskul.media.map(media=><img  src={media.url} alt={ekskul.name}/>)}
+
+            <div className="flex gap-10">
+              <Sidebar className="md:flex-none md:w-64">
+                <Sidebar.Items>
+                  <Sidebar.ItemGroup>
+                    {
+                      data.map((ekskul,index)=>(
+                        <Sidebar.Item href="#" onClick={()=>setActiveTab(index)} active={index == activeTab ? true : false} theme={{active:'bg-green-300 dark:bg-green-700'}}>
+                          {ekskul.name}                        
+                        </Sidebar.Item>
+                      ))
+                    }
+                  </Sidebar.ItemGroup>
+                </Sidebar.Items>
+              </Sidebar>
+              <div className="flex-auto">
+                <p>{data[activeTab].description}</p>
+
+                <LightGallery elementClassNames="grid grid-cols-2 md:grid-cols-4 gap-4 mt-4">
+                  {data[activeTab].media.map(media=>(
+                      <img className="h-auto max-w-full rounded-lg hover:cursor-pointer" src={media.url}/>
+                  ))}
+                </LightGallery>
               </div>
-            ))
+            </div>
+
           )}
+          
         </div>
     </>
   );
