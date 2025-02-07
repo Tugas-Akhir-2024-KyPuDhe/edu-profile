20b6970a29fef082154df5c41cddc91a7071e77c
Rizky Fadillah <fadillahrizki16@gmail.com>
2025-02-07 10:06:11 +0700

    feat: change bold in about

--- src/pages/tentang/index.tsx
+++ src/pages/tentang/index.tsx
@@ -13,7 +13,7 @@ export const Tentang = () => {
       <MyBanner title="Tentang Sekolah" currentPage="Tentang Sekolah" beforePages={[{title: "Beranda", href: "/"}]} />
 
       <div className="container py-10 gap-10 flex flex-col ">
-        <p className="text-pretty text-xl font-semibold text-gray-900 text-justify my-auto html-content">{parse(schoolConfig.about)}</p>
+        <p className="text-pretty text-lg text-gray-900 text-justify my-auto html-content">{parse(schoolConfig.about)}</p>
       </div>
     </>
   );
